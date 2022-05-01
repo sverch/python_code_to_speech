566cf769b403aa5465c2a4509e590708834f44bf
Shaun Verch <githubpublic@shaunverch.com>
2022-05-01 17:06:25 -0400

    First working prototype

---
+++ hello.py
@@ -0,0 +1,7 @@
+def foo(a1, a2):
+    print(a1)
+    print(a2)
+
+print("Hello, world!")
+print(1)
+foo("a1", "a2")
--- python_code_to_speech.py
+++ python_code_to_speech.py
@@ -6,14 +6,19 @@ Python Code To Speech: Reads python code aloud
 """
 import os
 import subprocess
+import ast
+from num2words import num2words
 import click
 from gtts import gTTS
 
 TEXT_TO_SPEECH_METHODS = ['gtts', 'espeak']
 
+### Code To Do Text To Speech ###
+# Text to speech support, implemented from: https://pythonbasics.org/text-to-speech/
+
 def execute_unix(inputcommand):
     """
-    Execute a command and fail
+    Execute an external command and return the output.
     """
     with subprocess.Popen(inputcommand, stdout=subprocess.PIPE, shell=True) as process:
         (output, _stderr) = process.communicate()
@@ -31,7 +36,7 @@ def read_text_aloud(text, strategy="gtts"):
         # initialize tts, create mp3 and play
         tts = gTTS(text, 'com')
         tts.save(file)
-        os.system("mpg123 " + file)
+        os.system("mpg123 -q " + file)
         os.remove(file)
     elif strategy == "espeak":
         # create wav file
@@ -44,6 +49,85 @@ def read_text_aloud(text, strategy="gtts"):
     else:
         raise Exception(f"Unrecognized strategy {strategy}")
 
+### Code To Parse Python Code ###
+# Uses this python library that reads the text and gives us a structured "Abstract Syntax Tree"
+# representing our python program: https://docs.python.org/3/library/ast.html
+
+# This code all copied from
+# https://stackoverflow.com/questions/1515357/simple-example-of-how-to-use-ast-nodevisitor
+
+class MyVisitor(ast.NodeVisitor):
+    """
+    Custom visitor to walk the Abstract Syntax Tree that represents the parsed version of our python
+    code and generate a human comprehensible text string.
+    """
+    def __init__(self, *args, **kwargs):
+        self.code_description = ""
+        super().__init__(*args, **kwargs)
+
+    def visit(self, node):
+        ast.NodeVisitor.visit(self, node)
+        return self.code_description
+
+    def generic_visit(self, node):
+        # print(type(node).__name__)
+        ast.NodeVisitor.generic_visit(self, node)
+
+    # pylint: disable=invalid-name, missing-function-docstring
+    def visit_Call(self, node):
+
+        # See documentation of https://docs.python.org/3/library/ast.html#ast.Call to figure out
+        # what properties this has.
+        line_number = node.lineno
+        name = node.func.id
+        arguments = node.args
+        number_of_arguments = len(arguments)
+        number_of_arguments_string = num2words(number_of_arguments)
+        arguments_string = "arguments" if number_of_arguments > 1 else "argument"
+
+        # Add a description of this function to our output.
+        self.code_description += (
+            f"At line {line_number} we are calling a function named "
+            f"\"{name}\" with "
+            f"{number_of_arguments_string} {arguments_string}.\n"
+        )
+
+        # Now lets add a description of the arguments.
+        for (index, argument) in enumerate(arguments):
+
+            # Indexes start at zero, but we don't want to say "zeroeth" for the first argument.
+            whichth = num2words(index + 1, to="ordinal")
+
+            if isinstance(argument, ast.Constant):
+                self.code_description += (
+                        f"The {whichth} argument to {name} is a {type(argument).__name__}, "
+                        f"with a type of {type(argument.value).__name__} "
+                        f"and a value of {argument.value}\n"
+                    )
+            else:
+                self.code_description += (
+                        f"The {whichth} argument to {name} is a {type(argument).__name__}, "
+                        "which is something I don't recognize yet\n"
+                    )
+                continue
+
+        ast.NodeVisitor.generic_visit(self, node)
+
+def python_code_to_text_description(python_code):
+    """
+    Takes as input text representing some python code and returns text describing that code.
+    """
+    parsed_python_code = ast.parse(python_code)
+
+    # This will dump out the structure, nice for debugging
+    # print(ast.dump(parsed_python_code, indent=4))
+
+    visitor = MyVisitor()
+
+    python_code_description = visitor.visit(parsed_python_code)
+    return python_code_description
+
+
 @click.command(context_settings={'show_default': True})
 @click.argument('filename')
 @click.option('--text-to-speech-method', type=click.Choice(TEXT_TO_SPEECH_METHODS), default="gtts")
@@ -52,11 +136,28 @@ def python_code_to_speech(filename, text_to_speech_method):
     Main command implementation
     """
     with open(filename, "r", encoding="utf8") as file_to_read:
-        text = file_to_read.read()
+        python_code_lines = file_to_read.readlines()
+        python_code = "".join(python_code_lines)
+
+        # Add line numbers to what we print, and also add one because python indexes start at zero
+        python_code_with_line_numbers = "".join([
+            f"{line_number + 1}: {code_line}"
+            for (line_number, code_line) in enumerate(python_code_lines)
+            ])
+
+        # Print what we are describing
+        print("Describing Python Code:\n")
+        print(python_code_with_line_numbers)
+        print("\n")
+
+        # Get the text description of our code
+        text_description = python_code_to_text_description(python_code)
+
+        # Read the code aloud using the selected method
         if text_to_speech_method == "gtts":
-            read_text_aloud(text, strategy="gtts")
+            read_text_aloud(text_description, strategy="gtts")
         elif text_to_speech_method == "espeak":
-            read_text_aloud(text, strategy="espeak")
+            read_text_aloud(text_description, strategy="espeak")
 
 
 if __name__ == '__main__':
