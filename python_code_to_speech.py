a4a5e2e8216d08e072f33b00644c5959ddcd06c5
Shaun Verch <githubpublic@shaunverch.com>
2022-05-01 15:50:54 -0400

    Add in espeak support

--- python_code_to_speech.py
+++ python_code_to_speech.py
@@ -1,9 +1,46 @@
 """
 Python Code To Speech: Reads python code aloud
+
+- Text to speech support from https://pythonbasics.org/text-to-speech/
+- Python ast support from https://docs.python.org/3/library/ast.html
 """
 import os
+import subprocess
 import click
 from gtts import gTTS
+
+def execute_unix(inputcommand):
+    """
+    Execute a command and fail
+    """
+    with subprocess.Popen(inputcommand, stdout=subprocess.PIPE, shell=True) as process:
+        (output, _stderr) = process.communicate()
+        return output
+
+def read_text_aloud(text, strategy="gtts"):
+    """
+    Read text aloud using the given strategy.
+
+    Assumes that a valid strategy is passed in and throws an exception otherwise.
+    """
+    if strategy == "gtts":
+        file = "file.mp3"
+
+        # initialize tts, create mp3 and play
+        tts = gTTS(text, 'com')
+        tts.save(file)
+        os.system("mpg123 " + file)
+        os.remove(file)
+    elif strategy == "espeak":
+        # create wav file
+        # w = 'espeak -w temp.wav "%s" 2>>/dev/null' % text
+        # execute_unix(w)
+
+        # tts using espeak
+        command = f'espeak -ven+f3 -k5 -s150 --punct="<characters>" "{text}" 2>>/dev/null'
+        execute_unix(command)
+    else:
+        raise Exception(f"Unrecognized strategy {strategy}")
 
 @click.group()
 def python_code_to_speech():
@@ -14,22 +51,14 @@ def python_code_to_speech():
 @python_code_to_speech.command()
 def gtts():
     '''Command on python_code_to_speech'''
-
-    # define variables
     text = "escape with plane"
-    file = "file.mp3"
-
-    # initialize tts, create mp3 and play
-    tts = gTTS(text, 'com')
-    tts.save(file)
-    os.system("mpg123 " + file)
+    read_text_aloud(text, strategy="gtts")
 
 @python_code_to_speech.command()
-def cmd2():
+def espeak():
     '''Command on python_code_to_speech'''
-    click.echo('python_code_to_speech cmd2')
-
-
+    text = "Say something in natural language."
+    read_text_aloud(text, strategy="espeak")
 
 
 if __name__ == '__main__':
