a54d8db01a21b31421672f1f1c2546667ff2d64a
Shaun Verch <githubpublic@shaunverch.com>
2022-05-01 15:42:17 -0400

    Proof of concept of google translate tts

--- python_code_to_speech.py
+++ python_code_to_speech.py
@@ -1,18 +1,36 @@
+"""
+Python Code To Speech: Reads python code aloud
+"""
+import os
 import click
+from gtts import gTTS
 
 @click.group()
 def python_code_to_speech():
-    pass
+    """
+    Group of subcommands
+    """
 
 @python_code_to_speech.command()
-def cmd1():
+def gtts():
     '''Command on python_code_to_speech'''
-    click.echo('python_code_to_speech cmd1')
+
+    # define variables
+    text = "escape with plane"
+    file = "file.mp3"
+
+    # initialize tts, create mp3 and play
+    tts = gTTS(text, 'com')
+    tts.save(file)
+    os.system("mpg123 " + file)
 
 @python_code_to_speech.command()
 def cmd2():
     '''Command on python_code_to_speech'''
     click.echo('python_code_to_speech cmd2')
+
+
+
 
 if __name__ == '__main__':
     python_code_to_speech()
